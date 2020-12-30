4f5f4aaab8ee651a956b6e2bafccf3ab44b1895a
zzdhxm12 <zzdhxm12@naver.com>
2020-12-30 22:27:38 +0900

    COVID19_database 기초설계

---
+++ COVID19_database/SQL.sql
@@ -0,0 +1,28 @@
+DROP DATABASE covid19;
+CREATE DATABASE covid19;
+USE covid19;
+CREATE TABLE day_sido (
+	stdDay varchar(100) not null,
+    sido varchar(100) not null,
+    defCnt int not null,
+    defTrans int not null,
+    deathCnt int not null,
+    deathTrans int not null
+);
+CREATE TABLE total_sido (
+	defCnt int not null,
+    deathCnt int not null
+);
+CREATE TABLE day_global (
+	stdDay varchar(100) not null,
+    areaName varchar(200) not null,
+    nationName varchar(200) not null,
+    dayDefCnt int not null,
+    dayDefTrans int not null,
+    dayDeathCnt int not null,
+    dayDeathTrans int not null
+);
+CREATE TABLE total_global (
+	defCnt int not null,
+    deathCnt int not null
+);
