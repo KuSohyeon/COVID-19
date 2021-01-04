42b17a78aaa8d20ce180d0b662f65d381987dc39
kusohyeon <kusohyeon@gmail.com>
2021-01-04 22:21:53 +0900

    vue GlobalList 작업 중

---
+++ COVID19_vue/src/components/world/GlobalList.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-container>
+        <v-data-table
+            :headers="headers"
+            :items="desserts"
+            :items-per-page="5"
+            class="elevation-1"
+        ></v-data-table>
+    </v-container>
+</template>
+<script>
+import axios from 'axios';
+export default {
+     name: 'GlobalList',
+    data () {
+      return {
+        headers: [
+          { text: '지역', value: 'nation' },
+          { text: '총 확진자', value: 'defCnt' },
+          { text: '오늘 확진자', value: 'dayDefCnt' },
+          { text: '총 사망자', value: 'DeathCnt' },
+          { text: '오늘 사망자', value: 'dayDeathCnt' },
+        ],
+        items: [],
+      }
+    },
+    created() {
+        axios
+            .get('http://localhost:8000/covid/global/today/countries')
+            .then(({ data }) => {
+                this.items = data;
+            })
+            .catch(() => {
+                // console.log(error);
+            });
+    }
+  
+}
+</script>
+<style scoped>
+</style>
--- COVID19_vue/src/views/World.vue
+++ COVID19_vue/src/views/World.vue
@@ -1,11 +1,15 @@
 <template>
      <v-container>
            <h1 class="white--text">해외</h1>
+           <global-list></global-list>
     </v-container>
 </template>
 <script>
+import GlobalList from '@/components/world/GlobalList.vue';
 export default {
-    
+    components: {
+        GlobalList
+    }
 }
 </script>
 <style scoped>
